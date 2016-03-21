093d9487e7abbc804d89a08558f11fefd0bdd8e1
mcmyffin <dimitri.meier@haw-haburg.de>
2016-03-21 09:50:07 +0100

    working version

--- PingPong/ping_bottle.py
+++ PingPong/ping_bottle.py
@@ -1,29 +1,44 @@
-from bottle import Bottle, redirect, request, run # or route
+from bottle import Bottle, redirect, request, run,HTTPResponse ,response;# or route
+from threading import Thread
 import requests
 import time
 
-rem_adr = "http://127.0.0.1:5000/service/sendPing"
+rem_adr = "http://141.22.86.37:8080/service/sendPing"
 app = Bottle()
 
 
 
+class Message():
+    message = ""
+
+    def getMessage(self):
+        self.message = self.message + "<b>Ping sent</b><br>"
+        return self.message
+
+
+message = Message()
+
 @app.get('/service/sendPing')
 def sendPing():
-    time.sleep(1)
+    time.sleep(2)
     print("send ping to "+rem_adr)
-    requests.post(url=rem_adr)
-    return "OK SENDED"
 
+    def postPing():
+        requests.post(url=rem_adr)
+
+    t = Thread(target = postPing, args=())
+    t.start()
+    return message.getMessage()
 
 
 @app.post('/service/sendPong')
-def recievePong():
-    time.sleep(2)
-    print("RECIEVE PONG")
-    time.sleep(0.5)
+def receivePong():
+
+    print("RECEIVE PONG")
+    # time.sleep(0.5)
     return sendPing()
 
 
-app.run(host="127.0.0.1", port="8080")
+app.run(host="141.22.89.39", port="8081")
 
 
--- PingPong/pong_bottle.py
+++ PingPong/pong_bottle.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, redirect, request, run # or route
+from bottle import Bottle, HTTPResponse
 import requests
 import time
 
@@ -8,9 +8,10 @@ adr = "http://127.0.0.1:8080/service/sendPong"
 @app.post('/service/sendPing')
 def receivePingSendPong():
     print("RECEIVE PING")
+
     time.sleep(1.5)
     print("SEND PONG to : "+adr)
-    requests.post(url=adr)
+    # requests.post(url=adr)
     return "RECEIVED PING AND SEND PONG"
 
 app.run(host="127.0.0.1", port="5000")
